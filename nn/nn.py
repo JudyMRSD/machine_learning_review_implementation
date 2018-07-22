265087e2a41eee8a65b311697bc24c911c358fda
Jin Zhu <jinz1@andrew.cmu.edu>
2018-07-22 12:15:39 -0400

    gradient descent finished

--- nn/nn.py
+++ nn/nn.py
@@ -1,5 +1,4 @@
 # https://github.com/peterroelants/peterroelants.github.io/blob/master/notebooks/neural_net_implementation/neural_network_implementation_part01.ipynb
-
 # Python imports
 import numpy  # Matrix and vector computation package
 import matplotlib.pyplot as plt  # Plotting library
@@ -8,20 +7,21 @@ import matplotlib.pyplot as plt  # Plotting library
 # Set the seed of the numpy random number generator so that the tutorial is reproducable
 numpy.random.seed(seed=1)
 
-
 class basic_nn():
     def __init__(self):
         pass
-
     # Generate the target values t from x with small gaussian noise so the estimation won't
     # be perfect.
     # Define a function f that represents the line that generates t without noise
 
     def f(self, x):
         return x * 2
+
     def forward(self, x, w):
         return x*w
+
     def cost(self,y, t):
+        # can also use mean, but with different learning rate
         return ((t-y)**2).sum()
 
     def gradient(self, w, x, t):
@@ -55,6 +55,22 @@ class test_nn():
         for i in range(0, len(w_cost)):
             print('w({}): {:.4f} \t cost: {:.4f}'.format(i, w_cost[i][0], w_cost[i][1]))
 
+        # Plot the first 2 gradient descent updates
+        plt.plot(self.ws, self.cost_ws, 'r-')  # Plot the error curve
+        # Plot the updates
+        for i in range(0, len(w_cost) - 2):
+            w1, c1 = w_cost[i]
+            w2, c2 = w_cost[i + 1]
+            plt.plot(w1, c1, 'bo')
+            plt.plot([w1, w2], [c1, c2], 'b-')
+            plt.text(w1, c1 + 0.5, '$w({})$'.format(i))
+            # Show figure
+        plt.xlabel('$w$', fontsize=15)
+        plt.ylabel('$\\xi$', fontsize=15)
+        plt.title('Gradient descent updates plotted on cost function')
+        plt.grid()
+        plt.savefig("./result/gradient_descent.png")
+
     def define_data(self):
         # Define the vector of input samples as x, with 20 values sampled from a uniform distribution
         # between 0 and 1
@@ -83,16 +99,16 @@ class test_nn():
     def test_weight_function(self):
         # Plot the cost vs the given weight w
         # Define a vector of weights for which we want to plot the cost
-        weights = numpy.linspace(0, 4, num=100)  # weight values
+        self.ws = numpy.linspace(0, 4, num=100)  # weight values
         # cost_ws = numpy.vectorize(lambda w: self.cost(self.nn(self.x, w), self.t))(ws)  # cost for each weight in ws
-        cost_ws = numpy.zeros_like(weights)
+        self.cost_ws = numpy.zeros_like(self.ws)
 
-        for i in range (weights.shape[0]):
-            pred = self.nn.forward(self.x, weights[i])
+        for i in range (self.ws.shape[0]):
+            pred = self.nn.forward(self.x, self.ws[i])
             print("pred", pred)
-            cost_ws[i] = self.nn.cost(pred, self.t)
+            self.cost_ws[i] = self.nn.cost(pred, self.t)
         # Plot
-        plt.plot(weights, cost_ws, 'r-')
+        plt.plot(self.ws, self.cost_ws, 'r-')
         plt.xlabel('$w$', fontsize=15)
         plt.ylabel('$\\xi$', fontsize=15)
         plt.title('cost vs. weight')
